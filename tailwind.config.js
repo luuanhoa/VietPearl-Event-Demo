92c4f2130ed9cefd7b069ebf6c3d9d7d96a0095f
luuanhoa <luuanhoa@gmail.com>
2025-09-02 16:16:39 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        vpBlue: "#0D62A8",
+        vpAqua: "#0FB3B3",
+        vpGold: "#D1A24B"
+      }
+    }
+  },
+  plugins: []
+};
